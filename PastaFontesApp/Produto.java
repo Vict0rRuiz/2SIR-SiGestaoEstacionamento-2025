fa602336fd685bc9d6ae669e7a85f49519e11260
Victor Ruiz <132236008+Vict0rRuiz@users.noreply.github.com>
2025-04-08 22:37:56 -0300

    Adicao de texto de exemplo do uso da classe
    Adicao de texto de exemplo do que a classe de objetos guarda

--- PastaFontesApp/Produto.java
+++ PastaFontesApp/Produto.java
@@ -1,34 +1,34 @@
+// Classe de objetos que guarda informacoes sobre produtos e servicos oferecidos no estacionamento
+// Produto e servicos sao, por exemplo: lavagem de veiculo; estacionar; troca de oleo
 
 public class Produto {
 
 	private double peso;
 	private double altura;
-	
+
 	public Produto() {
-		
+
 	}
-	
+
 	public Produto(double peso, double altura) {
-		this.peso=peso;
-		this.altura=altura;
+		this.peso = peso;
+		this.altura = altura;
 	}
-	
+
 	public double getPeso() {
 		return peso;
 	}
-	
+
 	public void setPeso(double peso) {
 		this.peso = peso;
 	}
-	
+
 	public double getAltura() {
 		return altura;
 	}
-	
+
 	public void setAltura(double altura) {
 		this.altura = altura;
 	}
-	
-	
-	
+
 }
